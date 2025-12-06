447ac399aa7096e0fa7c9d73d010346251042c62
NguyenTV-UIT <truongvinhnguyen2k6@gmail.com>
2025-12-07 01:35:12 +0700

    Cau4

--- Lab4/Cau4/Cinema.Designer.cs
+++ Lab4/Cau4/Cinema.Designer.cs
@@ -40,6 +40,32 @@
             cbb_movieSelection = new ComboBox();
             label4 = new Label();
             btn_bookticket = new Button();
+            tb_email = new TextBox();
+            label5 = new Label();
+            cb_A1 = new CheckBox();
+            cb_A2 = new CheckBox();
+            cb_B2 = new CheckBox();
+            cb_B1 = new CheckBox();
+            cb_C2 = new CheckBox();
+            cb_C1 = new CheckBox();
+            cb_A4 = new CheckBox();
+            cb_A3 = new CheckBox();
+            cb_B4 = new CheckBox();
+            cb_B3 = new CheckBox();
+            cb_C4 = new CheckBox();
+            cb_C3 = new CheckBox();
+            cb_C5 = new CheckBox();
+            cb_B5 = new CheckBox();
+            cb_A5 = new CheckBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
             SuspendLayout();
             // 
             // cbb_cinemaUrl
@@ -73,7 +99,7 @@
             // 
             progressBarDD.Location = new Point(231, 88);
             progressBarDD.Name = "progressBarDD";
-            progressBarDD.Size = new Size(500, 23);
+            progressBarDD.Size = new Size(500, 34);
             progressBarDD.TabIndex = 3;
             // 
             // btn_mvnowshwing
@@ -88,7 +114,7 @@
             // 
             // tb_name
             // 
-            tb_name.Location = new Point(231, 148);
+            tb_name.Location = new Point(231, 168);
             tb_name.Name = "tb_name";
             tb_name.Size = new Size(500, 31);
             tb_name.TabIndex = 5;
@@ -96,7 +122,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(35, 148);
+            label2.Location = new Point(35, 168);
             label2.Name = "label2";
             label2.Size = new Size(162, 25);
             label2.TabIndex = 6;
@@ -105,7 +131,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(35, 211);
+            label3.Location = new Point(35, 219);
             label3.Name = "label3";
             label3.Size = new Size(44, 25);
             label3.TabIndex = 7;
@@ -113,7 +139,7 @@
             // 
             // tb_phone
             // 
-            tb_phone.Location = new Point(231, 211);
+            tb_phone.Location = new Point(231, 219);
             tb_phone.Name = "tb_phone";
             tb_phone.Size = new Size(500, 31);
             tb_phone.TabIndex = 8;
@@ -121,7 +147,7 @@
             // cbb_movieSelection
             // 
             cbb_movieSelection.FormattingEnabled = true;
-            cbb_movieSelection.Location = new Point(231, 273);
+            cbb_movieSelection.Location = new Point(231, 321);
             cbb_movieSelection.Name = "cbb_movieSelection";
             cbb_movieSelection.Size = new Size(500, 33);
             cbb_movieSelection.TabIndex = 9;
@@ -129,7 +155,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(35, 273);
+            label4.Location = new Point(35, 321);
             label4.Name = "label4";
             label4.Size = new Size(52, 25);
             label4.TabIndex = 10;
@@ -137,7 +163,7 @@
             // 
             // btn_bookticket
             // 
-            btn_bookticket.Location = new Point(231, 341);
+            btn_bookticket.Location = new Point(231, 389);
             btn_bookticket.Name = "btn_bookticket";
             btn_bookticket.Size = new Size(112, 34);
             btn_bookticket.TabIndex = 11;
@@ -145,11 +171,269 @@
             btn_bookticket.UseVisualStyleBackColor = true;
             btn_bookticket.Click += btn_bookticket_Click;
             // 
+            // tb_email
+            // 
+            tb_email.Location = new Point(231, 270);
+            tb_email.Name = "tb_email";
+            tb_email.Size = new Size(500, 31);
+            tb_email.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(35, 270);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 25);
+            label5.TabIndex = 13;
+            label5.Text = "Email";
+            // 
+            // cb_A1
+            // 
+            cb_A1.AutoSize = true;
+            cb_A1.Location = new Point(843, 235);
+            cb_A1.Name = "cb_A1";
+            cb_A1.Size = new Size(22, 21);
+            cb_A1.TabIndex = 14;
+            cb_A1.UseVisualStyleBackColor = true;
+            // 
+            // cb_A2
+            // 
+            cb_A2.AutoSize = true;
+            cb_A2.Location = new Point(885, 235);
+            cb_A2.Name = "cb_A2";
+            cb_A2.Size = new Size(22, 21);
+            cb_A2.TabIndex = 15;
+            cb_A2.UseVisualStyleBackColor = true;
+            // 
+            // cb_B2
+            // 
+            cb_B2.AutoSize = true;
+            cb_B2.Location = new Point(885, 276);
+            cb_B2.Name = "cb_B2";
+            cb_B2.Size = new Size(22, 21);
+            cb_B2.TabIndex = 17;
+            cb_B2.UseVisualStyleBackColor = true;
+            // 
+            // cb_B1
+            // 
+            cb_B1.AutoSize = true;
+            cb_B1.Location = new Point(843, 276);
+            cb_B1.Name = "cb_B1";
+            cb_B1.Size = new Size(22, 21);
+            cb_B1.TabIndex = 16;
+            cb_B1.UseVisualStyleBackColor = true;
+            // 
+            // cb_C2
+            // 
+            cb_C2.AutoSize = true;
+            cb_C2.Location = new Point(885, 318);
+            cb_C2.Name = "cb_C2";
+            cb_C2.Size = new Size(22, 21);
+            cb_C2.TabIndex = 19;
+            cb_C2.UseVisualStyleBackColor = true;
+            // 
+            // cb_C1
+            // 
+            cb_C1.AutoSize = true;
+            cb_C1.Location = new Point(843, 318);
+            cb_C1.Name = "cb_C1";
+            cb_C1.Size = new Size(22, 21);
+            cb_C1.TabIndex = 18;
+            cb_C1.UseVisualStyleBackColor = true;
+            // 
+            // cb_A4
+            // 
+            cb_A4.AutoSize = true;
+            cb_A4.Location = new Point(968, 235);
+            cb_A4.Name = "cb_A4";
+            cb_A4.Size = new Size(22, 21);
+            cb_A4.TabIndex = 21;
+            cb_A4.UseVisualStyleBackColor = true;
+            // 
+            // cb_A3
+            // 
+            cb_A3.AutoSize = true;
+            cb_A3.Location = new Point(926, 235);
+            cb_A3.Name = "cb_A3";
+            cb_A3.Size = new Size(22, 21);
+            cb_A3.TabIndex = 20;
+            cb_A3.UseVisualStyleBackColor = true;
+            // 
+            // cb_B4
+            // 
+            cb_B4.AutoSize = true;
+            cb_B4.Location = new Point(968, 276);
+            cb_B4.Name = "cb_B4";
+            cb_B4.Size = new Size(22, 21);
+            cb_B4.TabIndex = 23;
+            cb_B4.UseVisualStyleBackColor = true;
+            // 
+            // cb_B3
+            // 
+            cb_B3.AutoSize = true;
+            cb_B3.Location = new Point(926, 276);
+            cb_B3.Name = "cb_B3";
+            cb_B3.Size = new Size(22, 21);
+            cb_B3.TabIndex = 22;
+            cb_B3.UseVisualStyleBackColor = true;
+            // 
+            // cb_C4
+            // 
+            cb_C4.AutoSize = true;
+            cb_C4.Location = new Point(968, 318);
+            cb_C4.Name = "cb_C4";
+            cb_C4.Size = new Size(22, 21);
+            cb_C4.TabIndex = 25;
+            cb_C4.UseVisualStyleBackColor = true;
+            // 
+            // cb_C3
+            // 
+            cb_C3.AutoSize = true;
+            cb_C3.Location = new Point(926, 318);
+            cb_C3.Name = "cb_C3";
+            cb_C3.Size = new Size(22, 21);
+            cb_C3.TabIndex = 24;
+            cb_C3.UseVisualStyleBackColor = true;
+            // 
+            // cb_C5
+            // 
+            cb_C5.AutoSize = true;
+            cb_C5.Location = new Point(1008, 318);
+            cb_C5.Name = "cb_C5";
+            cb_C5.Size = new Size(22, 21);
+            cb_C5.TabIndex = 28;
+            cb_C5.UseVisualStyleBackColor = true;
+            // 
+            // cb_B5
+            // 
+            cb_B5.AutoSize = true;
+            cb_B5.Location = new Point(1008, 276);
+            cb_B5.Name = "cb_B5";
+            cb_B5.Size = new Size(22, 21);
+            cb_B5.TabIndex = 27;
+            cb_B5.UseVisualStyleBackColor = true;
+            // 
+            // cb_A5
+            // 
+            cb_A5.AutoSize = true;
+            cb_A5.Location = new Point(1008, 235);
+            cb_A5.Name = "cb_A5";
+            cb_A5.Size = new Size(22, 21);
+            cb_A5.TabIndex = 26;
+            cb_A5.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(793, 158);
+            label6.Name = "label6";
+            label6.Size = new Size(93, 25);
+            label6.TabIndex = 29;
+            label6.Text = "Chọn ghế:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(793, 232);
+            label7.Name = "label7";
+            label7.Size = new Size(24, 25);
+            label7.TabIndex = 30;
+            label7.Text = "A";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(793, 272);
+            label8.Name = "label8";
+            label8.Size = new Size(22, 25);
+            label8.TabIndex = 31;
+            label8.Text = "B";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(793, 314);
+            label9.Name = "label9";
+            label9.Size = new Size(23, 25);
+            label9.TabIndex = 32;
+            label9.Text = "C";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(843, 193);
+            label10.Name = "label10";
+            label10.Size = new Size(22, 25);
+            label10.TabIndex = 33;
+            label10.Text = "1";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(885, 193);
+            label11.Name = "label11";
+            label11.Size = new Size(22, 25);
+            label11.TabIndex = 34;
+            label11.Text = "2";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(926, 193);
+            label12.Name = "label12";
+            label12.Size = new Size(22, 25);
+            label12.TabIndex = 35;
+            label12.Text = "3";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(968, 193);
+            label13.Name = "label13";
+            label13.Size = new Size(22, 25);
+            label13.TabIndex = 36;
+            label13.Text = "4";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(1008, 193);
+            label14.Name = "label14";
+            label14.Size = new Size(22, 25);
+            label14.TabIndex = 37;
+            label14.Text = "5";
+            // 
             // Cinema
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1041, 415);
+            ClientSize = new Size(1176, 483);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(cb_C5);
+            Controls.Add(cb_B5);
+            Controls.Add(cb_A5);
+            Controls.Add(cb_C4);
+            Controls.Add(cb_C3);
+            Controls.Add(cb_B4);
+            Controls.Add(cb_B3);
+            Controls.Add(cb_A4);
+            Controls.Add(cb_A3);
+            Controls.Add(cb_C2);
+            Controls.Add(cb_C1);
+            Controls.Add(cb_B2);
+            Controls.Add(cb_B1);
+            Controls.Add(cb_A2);
+            Controls.Add(cb_A1);
+            Controls.Add(label5);
+            Controls.Add(tb_email);
             Controls.Add(btn_bookticket);
             Controls.Add(label4);
             Controls.Add(cbb_movieSelection);
@@ -182,5 +466,31 @@
         private ComboBox cbb_movieSelection;
         private Label label4;
         private Button btn_bookticket;
+        private TextBox tb_email;
+        private Label label5;
+        private CheckBox cb_A1;
+        private CheckBox cb_A2;
+        private CheckBox cb_B2;
+        private CheckBox cb_B1;
+        private CheckBox cb_C2;
+        private CheckBox cb_C1;
+        private CheckBox cb_A4;
+        private CheckBox cb_A3;
+        private CheckBox cb_B4;
+        private CheckBox cb_B3;
+        private CheckBox cb_C4;
+        private CheckBox cb_C3;
+        private CheckBox cb_C5;
+        private CheckBox cb_B5;
+        private CheckBox cb_A5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
     }
 }
