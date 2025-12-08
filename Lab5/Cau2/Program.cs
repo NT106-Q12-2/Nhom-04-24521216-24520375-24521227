d96bf09f98329c1a3a1bb045e75364313f783b2d
duyhn <dannybenchanmark1111@gmail.com>
2025-12-09 02:27:41 +0700

    Update Program.cs

--- Lab5/Cau2/Program.cs
+++ Lab5/Cau2/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Windows.Forms;
 
-namespace NT106_LAB05
+namespace Cau2
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new Cau2());  
         }
     }
 }
