a5d3dab6c7bfcf077d84612a5e5e21715330165a
duyhn <dannybenchanmark1111@gmail.com>
2025-12-09 02:28:17 +0700

    Update Cau2.Designer.cs

--- Lab5/Cau2/Cau2.Designer.cs
+++ Lab5/Cau2/Cau2.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace NT106_LAB05
+namespace Cau2
 {
     partial class Cau2
     {
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label_totalvalue = new System.Windows.Forms.Label();
+            this.label_recentvalue = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Email = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.From = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // label_totalvalue
+            // 
+            this.label_totalvalue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_totalvalue.Location = new System.Drawing.Point(115, 238);
+            this.label_totalvalue.Name = "label_totalvalue";
+            this.label_totalvalue.Size = new System.Drawing.Size(100, 23);
+            this.label_totalvalue.TabIndex = 2;
+            this.label_totalvalue.Text = "Total: ";
+            // 
+            // label_recentvalue
+            // 
+            this.label_recentvalue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_recentvalue.Location = new System.Drawing.Point(398, 238);
+            this.label_recentvalue.Name = "label_recentvalue";
+            this.label_recentvalue.Size = new System.Drawing.Size(100, 23);
+            this.label_recentvalue.TabIndex = 3;
+            this.label_recentvalue.Text = "Recent:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(250, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(488, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(250, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(488, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(881, 83);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(140, 71);
+            this.btn_login.TabIndex = 6;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Email,
+            this.From,
+            this.Date});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(108, 291);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(913, 328);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Email
+            // 
+            this.Email.Text = "Email";
+            this.Email.Width = 300;
+            // 
+            // From
+            // 
+            this.From.Text = "From";
+            this.From.Width = 300;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 300;
+            // 
+            // Cau2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1114, 676);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_recentvalue);
+            this.Controls.Add(this.label_totalvalue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Cau2";
             this.Text = "Cau2";
+            this.Load += new System.EventHandler(this.Cau2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label_totalvalue;
+        private System.Windows.Forms.Label label_recentvalue;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Email;
+        private System.Windows.Forms.ColumnHeader From;
+        private System.Windows.Forms.ColumnHeader Date;
     }
 }
